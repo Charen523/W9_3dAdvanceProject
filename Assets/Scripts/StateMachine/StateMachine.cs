72680d3803190230740a488f547da1d18a09a49f
Chloe <144107013+Charen523@users.noreply.github.com>
2024-06-13 12:52:01 +0900

    [init]

---
+++ Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,27 @@
+﻿
+public abstract class StateMachine
+{
+    protected IState currentState;
+
+    public void ChangeState(IState state)
+    {
+        currentState?.Exit();
+        currentState = state;
+        currentState?.Enter();
+    }
+
+    public void HandleInput()
+    {
+        currentState?.HandleInput();
+    }
+
+    public void Update() //Monobehaviour 없어서 그 Update아님.
+    {
+        currentState?.Update();
+    }
+
+    public void PhysicsUpdate()
+    {
+        currentState?.PhysicsUpdate();
+    }
+}
